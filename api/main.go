b6eac348012276165b218f27e9f04575b655790b
Harshit Gaur <harshit2k2k@gmail.com>
2022-08-13 00:23:02 +0530

    Day 4

--- api/main.go
+++ api/main.go
@@ -1,4 +1,5 @@
 package main
+package routes
 
 import (
 	"fmt"
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,11 @@
 package routes
+package helpers import helpers
 
-import "time"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +19,32 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"x_rate_remaining"`
 	XRateLimitReset time.Duration `json:"x_rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	// Implementation of rate Limiter
+
+	// check if the input sent by user is actual URL
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
+	}
+
+	//localhost check : for domain error
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fibre.StatusServiceUnavailable).JSON(fiber.Map{"error": "Service is down :) "})
+	}
+
+	// enforce HTTPS, SSL connection
+
+	body.URL = helpers.EnforceHTTPS(body.URL)
+
 }
